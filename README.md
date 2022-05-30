5b5e8de74ffa8de9b6a44222daa92d242e402fd2
Tazkia Athariza Dhivara <tazkia.athariza.dhivara-2018@fst.unair.ac.id>
2022-05-30 09:19:17 +0700

    fixed the car filter in sectionCar.js (for searchPage) and modified README.md

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # Tazkia - Car Rental_React - Challenge 7
 
-## Getting Started with Create React App and Redux
+"Car Rental_React" was created for academic purpose, called Challenge Chapter 7 in Binar Academy - Fullstack Web Course. The aims of this challenge are to make react aplication (using reactJS), connect it to backend with HTTP protocols, and implement Redux to manage the data.
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app), using the [Redux](https://redux.js.org/) and [Redux Toolkit](https://redux-toolkit.js.org/) template.
-
-## Available Scripts
+## About 
+This website contains two major pages and built by using several third-party modules including bootstrap, jquery, react, react-dom, react-redux, react-router-dom, redux-toolkit, etc. So if you're about to run this program, you need to install those modules first. After that, run 'npm install' and 'npm start' in your terminal. 
 
 In the project directory, you can run:
 
@@ -21,28 +20,8 @@ You may also see any lint errors in the console.
 Launches the test runner in the interactive watch mode.\
 See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
 
-### `npm run build`
+## Version History
+Commit changes can be checked on https://gitlab.com/tazkia.athariza.dhivara-2018/tazkia-car-rental_react-challenge-7/-/commits/main
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+## Author
+Tazkia Athariza Dhivara
--- src/components/SectionCar.js
+++ src/components/SectionCar.js
@@ -17,32 +17,37 @@ function SectionCar() {
   const cars = useSelector(getAllCars);
   const carsStatus = useSelector((state) => state.cars.status);
   const error = useSelector((state) => state.cars.error);
-  const [carsFilter, setCarsFilter] = useState([])
-  const [capacity, setCapacity] = useState()
-  const [jam, setJam] = useState()
-  const [tanggal, setTanggal] = useState()
+  const [carsFilter, setCarsFilter] = useState([]);
+  const [capacity, setCapacity] = useState();
+  const [jam, setJam] = useState();
+  const [tanggal, setTanggal] = useState();
 
-  const doFilterCars = () => {
+  const doFilterCars = ( ) => {
+  
+    const filterCarResult =  cars.filter((item) => {
+      let dateTime = new Date(tanggal + " " + jam);
+      let epochTime = dateTime.getTime();
+      let date = new Date(item.availableAt);
+      let newDate = date.getTime();
 
-      const filterCarResult =  cars.filter(item => {
-        let dateTime = new Date(tanggal + " " + jam)
-        let date = new Date(item.availableAt);
-        let newDate = date.getTime()
-        const epochTime = dateTime.getTime()
-        let dateFilter = newDate < epochTime;
-        let capacityFilter = item.capacity <= capacity;
+      let availableFilter = item.available === true;
+      let dateFilter = newDate < epochTime;
+      let capacityFilter = item.capacity >= capacity;
+      return capacityFilter && dateFilter && availableFilter;
+    });
+    setCarsFilter(filterCarResult);
+    console.log(filterCarResult);
+  }
 
-        return capacityFilter && dateFilter;
-        })
-
-      setCarsFilter(filterCarResult);
-      console.log(filterCarResult);
-  };
+  const shows = ( ) => {
+    setCarsFilter(cars);
+  }
 
   useEffect (() => {
     if (carsStatus === 'idle') {
       dispatch(fetchCars());
     }
+
     console.log("Cars Status :", carsStatus);
     }, [carsStatus, dispatch]);
 
@@ -53,27 +58,23 @@ function SectionCar() {
     } else if (carsStatus === 'succeeded') {
       content = (
         <>
-            <div className="section">
-
                 <div id="section1">
-                          <div className="container-fluid pt-5 landingPage">
-                                  <div className="row">
-                                      <div className="col-md-6" id="texthome">
-                                          <h1><b>Sewa dan Rental Mobil Terbaik di kawasan Surabaya</b></h1>
-                                          <p>Selamat datang di Binar Car Rental. Kami Menyediakan mobil kualitas terbaik dengan harga terjangkau.Selalu siap melayani kebutuhanmu untuk sewa mobil selama 24 jam.</p>
-                                      </div>
-                                      <div className="col-md-6 mt-lg-5">
-                                          <img src={imgCar} id="ImgCar" class="img-fluid"/>
-                                      </div>
-                                  </div>
-                              </div>
+                    <div className="container-fluid pt-5 landingPage">
+                        <div className="row">
+                            <div className="col-md-6" id="texthome">
+                                <h1><b>Sewa dan Rental Mobil Terbaik di kawasan Surabaya</b></h1>
+                                <p>Selamat datang di Binar Car Rental. Kami Menyediakan mobil kualitas terbaik dengan harga terjangkau.Selalu siap melayani kebutuhanmu untuk sewa mobil selama 24 jam.</p>
+                            </div>
+                                <div className="col-md-6 mt-lg-5">
+                                  <img src={imgCar} id="ImgCar" class="img-fluid"/>
+                                </div>
+                          </div>
+                      </div>
                   </div>
 
                 <div className="container position-relative cariMobil">
-                <div className="formCariMobil">
-                  <form id="form" action="">
+                  <div className="formCariMobil">
                     <div className="row">
-
                       <div className="col-lg">
                         <label for="inputDriver" className="form-label">Tipe Driver</label>
                         <select className="form-select" placeholder="Pilih Tipe Driver" id="inputDriver" required>
@@ -82,62 +83,49 @@ function SectionCar() {
                           <option className="opsi" value="Tanpa Sopir">Tanpa Sopir (Lepas Kunci)</option>
                         </select>
                       </div>
-
                       <div className="col-lg">
                         <label for="inputDate" className="form-label">Tanggal</label>
                         <input type="date" className="form-control" placeholder="Pilih Tanggal" id="inputDate" onChange={(e) => setTanggal((e.target.value))} required/>
                       </div>
-
                       <div className="col-lg">
                         <label for="inputTime" className="form-label">Waktu Jemput</label>
                         <input type="time" id="inputTime" className="form-control" placeholder="00:00" onChange={(e) => setJam((e.target.value))} required/>
                       </div>
-
                       <div className="col-lg">
                         <label for="penumpang" className="form-label">Jumlah Penumpang</label>
                         <input id="jumlahPenumpang" type="number" className="form-control" placeholder="Jumlah Penumpang" onChange={(e) => setCapacity(Number(e.target.value))} required/>
                       </div>
-
                       <div className="col-lg">
                         <button className="btn btn-success" id="load-btn" onClick={doFilterCars}>Cari Mobil</button>
                       </div>
-
                     </div>
-                  </form>
                 </div>
-                </div>
+              </div>
 
-                <div className="container my-lg-2">
-
-                    <button id="clear-btn" className="btn btn-outline-danger">Hapus Pencarian</button>
-
-                    <div id="cars-container" className="row row-cols-1 row-cols-xs-1 row-cols-sm-1 row-cols-lg-3 row-cols-md-2 g-3 mb-5 mt-3">
-
+              <div className="container my-lg-2">
+                  <button id="clear-btn" className="btn btn-outline-primary" onClick={shows}>Tampilkan Semua Mobil</button>
+                  <div id="cars-container" className="row row-cols-1 row-cols-xs-1 row-cols-sm-1 row-cols-lg-3 row-cols-md-2 g-3 mb-5 mt-3">
                       {carsFilter.map(
-                        item => {
+                        (item) => {
                           return (
                             <div className="card col-lg-4 cardCars" key={item.id}>
                               <div className="card-body">
                                         <img src={item.image} className="imageCars"/>
-                                        <div className="txt-Tipe">{item.model}</div>
+                                        <div className="txt-Tipe"> {item.manufacture} {item.model}</div>
                                         <div className="txt-Harga">Rp {item.rentPerDay} / hari</div>
                                         <div className="txt-Caption">{item.description}</div>
                                         <div className="txt-Caption"><img src={iconUser}/>  {item.capacity}</div>
                                         <div className="txt-Caption"><img src={iconSetting}/>  {item.transmission}</div>
                                         <div className="txt-Caption"><img src={iconCalendar}/>    {item.year}</div>
-                                        <button type="submit" className="btn btn-success" id="btn-pilihMobil">Pilih Mobil</button>
                                 </div>
+                                <button type="submit" className="btn btn-success" id="btn-pilihMobil">Pilih Mobil</button>
                               </div> 
-                            )
+                            );
                           }
                         )
                       }
-
-
                     </div>
                 </div>
-
-            </div>
         </>
       );
 
